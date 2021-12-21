7de183c88a6395d0b2c98af468a74714c0698f1b
Koichi.Shimomura <unieye51@gmail.com>
2021-12-21 16:03:23 +0900

    section-012: モード設定のバグ回避

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,29 @@ const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + ( breakLine ? '\n' : ''))
 }
 
+const readline = async() => {
+  const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+  return input.trim()
+}
+
 const promptInput = async (text: string) => {
   printLine(`\n${text}\n> `, false)
-  
-  const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => 
-                          resolve(data.toString())))
+  return readline()
+}
 
-  return input.trim()
+const promptSelect = async (text:string, values: readonly string[]): Promise<string> => {
+  printLine(`\n${text}`)
+  values.forEach((value) => {
+    printLine(`- ${value}`)
+  })
+  printLine('> ', false)
+
+  const input = await readline()
+  if (values.includes(input)) {
+    return input
+  } else {
+    return promptSelect(text, values)
+  }
 }
 
 class HitAndBlow {
@@ -20,7 +36,7 @@ class HitAndBlow {
   private mode: Mode = 'normal'
 
   async setting() {
-    this.mode = await promptInput('モードを入力してください') as Mode
+    this.mode = await promptSelect('モードを入力してください。', ['normal', 'hard']) as Mode
     const answerLength = this.getAnswerLength()
 
     while(this.answer.length < answerLength) {
